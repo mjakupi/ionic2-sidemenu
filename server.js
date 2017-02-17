12d26a0592d86806c7b7ae76992c9164ad684eb3
mjakupi <mjakupi@outlook.com>
2017-02-17 17:12:17 +0100

    second commit

---
+++ server.js
@@ -0,0 +1,10 @@
+/**
+ * Created by mjakupi on 2/17/17.
+ */
+var express = require('express'),
+    app = express();
+app.use(express.static('www'));
+app.set('port', process.env.PORT || 5000);
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
