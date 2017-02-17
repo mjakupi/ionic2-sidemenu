1a3c341c553814dc3a77a9bed7d444f3aded7f49
mjakupi <mjakupi@outlook.com>
2017-02-17 17:32:20 +0100

    Dasboard Added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Nav, Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { Page1 } from '../pages/page1/page1';
-import { Page2 } from '../pages/page2/page2';
+import {DashboardPage} from "../pages/dashboard/dashboard";
 
 
 @Component({
@@ -21,11 +21,12 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Page One', component: Page1 },
-      { title: 'Page Two', component: Page2 }
+      { title: 'Dashboard', component: DashboardPage },
+      { title: 'Page One', component: Page1 }
     ];
 
   }
+
 
   initializeApp() {
     this.platform.ready().then(() => {
